4cc52975cf1af5e569841df954e70ebf96c15f51
xpcrtv <20889274+xpcrtv@users.noreply.github.com>
2020-10-25 17:42:25 +0500

    add: datepicker

--- src/widget.js
+++ src/widget.js
@@ -1,5 +1,18 @@
+import flatpickr from "flatpickr";
+import rangePlugin from "~/flatpickr/dist/plugins/rangePlugin";
+import "~/flatpickr/dist/themes/light.css";
+
 import "./styles/main.scss";
 
-const widget = () => console.log("widget");
+const widget = () => {
+  const dateDepartInput = document.querySelector('[name="date-from"]');
+  const dateReturnInput = document.querySelector('[name="date-to"]');
+  flatpickr(dateDepartInput, {
+    clickOpens: false,
+    disableMobile: true,
+    dateFormat: "d.m.Y",
+    plugins: [new rangePlugin({ input: dateReturnInput })],
+  });
+};
 
 export default widget;
